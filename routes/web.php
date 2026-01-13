eed7e3277c8f7f95bfca7d61564576e28e46bc1d
thiscesar <cesarldsousa@gmail.com>
2026-01-12 21:55:35 -0300

    temp: fake login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 declare(strict_types = 1);
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn (): Illuminate\Contracts\View\Factory | \Illuminate\Contracts\View\View => view('welcome'));
+
+Route::get('/login/{id}', fn ($id): Illuminate\Contracts\Auth\Authenticatable | false => Auth::loginUsingId($id));
