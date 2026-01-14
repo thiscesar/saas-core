5d9f299dff9231c02777470c80a2c56d96911d60
thiscesar <cesarldsousa@gmail.com>
2026-01-14 15:27:29 -0300

    feat: login route

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 declare(strict_types = 1);
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn (): Illuminate\Contracts\View\Factory | \Illuminate\Contracts\View\View => view('welcome'));
+
+Route::post('/login', LoginController::class)->name('login.store');
 
 Route::get('/login/{id}', fn ($id): Illuminate\Contracts\Auth\Authenticatable | false => Auth::loginUsingId($id));
