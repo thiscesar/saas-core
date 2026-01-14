62d5f5f320cea96895d45d70c62fbdc47dd276c2
thiscesar <cesarldsousa@gmail.com>
2026-01-14 15:47:48 -0300

    feat: pt_BR lang auth file

---
+++ lang/pt_BR/auth.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types = 1);
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Linguagem de Autenticação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de linguagem são usadas durante a autenticação para
+    | várias mensagens que precisamos exibir ao usuário. Você é livre para
+    | modificar essas linhas de linguagem de acordo com os requisitos da sua
+    | aplicação.
+    |
+    */
+
+    'failed'   => 'As credenciais fornecidas não correspondem aos nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+
+];
