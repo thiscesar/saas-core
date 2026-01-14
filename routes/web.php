4ace5856b68146307516f69884bc4fc25dab8c87
thiscesar <cesarldsousa@gmail.com>
2026-01-14 15:46:05 -0300

    fix: add missing login view route

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', fn (): Illuminate\Contracts\View\Factory | \Illuminate\Contracts\View\View => view('welcome'));
 
+Route::get('/login', fn (): Illuminate\Contracts\View\Factory | \Illuminate\Contracts\View\View => view('login'))->name('login');
 Route::post('/login', LoginController::class)->name('login.store');
 
 Route::get('/login/{id}', fn ($id): Illuminate\Contracts\Auth\Authenticatable | false => Auth::loginUsingId($id));
