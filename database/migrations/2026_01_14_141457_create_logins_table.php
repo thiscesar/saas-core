86b82307afb52b6ad4890214b9bb6417e2cb5cde
thiscesar <cesarldsousa@gmail.com>
2026-01-14 12:33:39 -0300

    refact: type hint migration

--- database/migrations/2026_01_14_141457_create_logins_table.php
+++ database/migrations/2026_01_14_141457_create_logins_table.php
@@ -13,7 +13,7 @@ return new class () extends Migration
      */
     public function up(): void
     {
-        Schema::create('logins', function (Blueprint $table) {
+        Schema::create('logins', function (Blueprint $table): void {
             $table->id();
             $table->foreignId('user_id')->constrained('users');
             $table->string('ip')->nullable();
