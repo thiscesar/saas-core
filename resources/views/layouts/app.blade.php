fceaa4990af1734b5dd7d17bad5676470b1ae63f
thiscesar <cesarldsousa@gmail.com>
2026-01-16 19:20:33 -0300

    style: collapse text

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
         {{-- Main Container --}}
         <x-main full-width>
             {{-- Sidebar --}}
-            <x-slot:sidebar drawer="main-drawer" collapsible class="bg-base-200">
+            <x-slot:sidebar drawer="main-drawer" collapsible collapse-text="Recolher"  class="bg-base-200">
                 <div class="flex h-full flex-col">
                     {{-- Brand --}}
                     <div class="mb-5 ml-5 pt-5">
