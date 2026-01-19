3d550eed56b3835a84ea7c6c20dcb1d4794b1826
thiscesar <cesarldsousa@gmail.com>
2026-01-19 13:23:53 -0300

    refact: getAvatarUrl User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Authenticatable implements Auditable
      */
     public function getAvatarUrl(): ?string
     {
-        return $this->attributes['avatar_url'] ?? null;
+        return $this->getAttributeValue('avatar_url');
     }
 
     /**
