e6f4117c19624cc883adebf74c7995e1174cf4a5
thiscesar <cesarldsousa@gmail.com>
2026-01-20 14:10:51 -0300

    fix: skip ngrok warning page

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,12 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['ngrok-skip-browser-warning'] = 'true';
+
+// Configure Livewire to skip ngrok warning page
+document.addEventListener('livewire:init', () => {
+    Livewire.hook('request', ({ options }) => {
+        options.headers = options.headers || {};
+        options.headers['ngrok-skip-browser-warning'] = 'true';
+    });
+});
